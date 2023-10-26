a9ad8444627d867e70b4e15586931c94c472168f
ashish sonam <ashishsonam@devtron.ai>
2023-10-26 14:25:29 +0530

    wip

--- pkg/util/Helper.go
+++ pkg/util/Helper.go
@@ -1,6 +1,15 @@
 package util
 
-import "reflect"
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"hash"
+	"hash/fnv"
+	coreV1 "k8s.io/api/core/v1"
+	"reflect"
+)
+
+const alphanums = "bcdfghjklmnpqrstvwxz2456789"
 
 func IsMapSubset(mapSet interface{}, mapSubset interface{}) bool {
 
@@ -29,4 +38,32 @@ func IsMapSubset(mapSet interface{}, mapSubset interface{}) bool {
 	}
 
 	return true
+}
+
+func ComputeHash(template *coreV1.PodTemplateSpec) string {
+	podTemplateSpecHasher := fnv.New32a()
+	DeepHashObject(podTemplateSpecHasher, *template)
+	return SafeEncodeString(fmt.Sprint(podTemplateSpecHasher.Sum32()))
+}
+
+func SafeEncodeString(s string) string {
+	r := make([]byte, len(s))
+	for i, b := range []rune(s) {
+		r[i] = alphanums[(int(b) % len(alphanums))]
+	}
+	return string(r)
+}
+
+func DeepHashObject(hasher hash.Hash, objectToWrite interface{}) {
+	hasher.Reset()
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
+	_, err := printer.Fprintf(hasher, "%#v", objectToWrite)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
