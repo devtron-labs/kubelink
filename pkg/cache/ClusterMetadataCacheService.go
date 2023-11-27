db1c2eff63d1dd13717c09b57337e7405851d896
Prakash Kumar <prakash.kumar@devtron.ai>
2023-11-27 18:11:48 +0530

    exposing ClusterCacheListPageSize in clustercacheconfig

--- pkg/cache/ClusterMetadataCacheService.go
+++ pkg/cache/ClusterMetadataCacheService.go
@@ -20,6 +20,7 @@ type ClusterCache interface {
 type ClusterCacheConfig struct {
 	ClusterIdList                 []int `env:"CLUSTER_ID_LIST" envSeparator:","`
 	ClusterCacheListSemaphoreSize int64 `env:"CLUSTER_CACHE_LIST_SEMAPHORE_SIZE" envDefault:"5"`
+	ClusterCacheListPageSize      int64 `env:"CLUSTER_CACHE_LIST_PAGE_SIZE" envDefault:"500"`
 }
 
 func GetClusterCacheConfig() (*ClusterCacheConfig, error) {
