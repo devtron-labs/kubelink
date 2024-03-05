c6fff541062a66748d3a092b87469599aea198d4
Prakash Kumar <prakash.kumar@devtron.ai>
2024-03-05 18:42:53 +0530

    include values.yaml in chart spec req

--- pkg/service/HelmAppService.go
+++ pkg/service/HelmAppService.go
@@ -953,6 +953,7 @@ func (impl HelmAppServiceImpl) GetNotes(ctx context.Context, request *client.Ins
 		MaxHistory:    0,    // limit the maximum number of revisions saved per release. Use 0 for no limit (default 10)
 		RepoURL:       request.ChartRepository.Url,
 		Version:       request.ChartVersion,
+		ValuesYaml:    request.ValuesYaml,
 	}
 	HelmTemplateOptions := &helmClient.HelmTemplateOptions{}
 	if request.K8SVersion != "" {
