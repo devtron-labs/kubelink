f9c4fd0e77fd1cf547d685ad0b8d35853c56fa94
Prakash Kumar <prakash.kumar@devtron.ai>
2024-03-14 18:19:41 +0530

    refactor

--- error/utils.go
+++ error/utils.go
@@ -6,27 +6,6 @@ import (
 	"strings"
 )
 
-// list of errors from helm
-const (
-	YAMLToJSONConversionError   = "error converting YAML to JSON"
-	ClusterUnreachableErrorMsg  = "cluster unreachable"
-	CrdPreconditionErrorMsg     = "ensure CRDs are installed first"
-	ArrayStringMismatchErrorMsg = "got array expected string"
-	NamespaceNotFoundErrorMsg   = "not found"
-	InvalidValueErrorMsg        = "Invalid value"
-	OperationInProgressErrorMsg = "another operation (install/upgrade/rollback) is in progress"
-)
-
-// list of internal errors
-const (
-	InternalClusterUnreachableErrorMsg  = "cluster unreachable"
-	InternalCrdPreconditionErrorMsg     = "ensure CRDs are installed first"
-	InternalArrayStringMismatchErrorMsg = "got array expected string"
-	InternalNamespaceNotFoundErrorMsg   = "namespace not found"
-	InternalInvalidValueErrorMsg        = "invalid value in manifest"
-	InternalOperationInProgressErrorMsg = "another operation (install/upgrade/rollback) is in progress"
-)
-
 func ConvertHelmErrorToInternalError(err error) error {
 	helmErrorInternalErrorMap := map[string]map[string]codes.Code{
 		ClusterUnreachableErrorMsg:  {InternalClusterUnreachableErrorMsg: codes.DeadlineExceeded},
