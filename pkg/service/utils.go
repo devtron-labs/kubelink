efd50a898db52dfccf3158be5df799cf00f63237
Prakash Kumar <prakash.kumar@devtron.ai>
2024-03-27 14:11:00 +0530

    refactoring:- small fix

--- pkg/service/utils.go
+++ pkg/service/utils.go
@@ -1,7 +1,10 @@
 package service
 
-import client "github.com/devtron-labs/kubelink/grpc"
+import (
+	client "github.com/devtron-labs/kubelink/grpc"
+	"strconv"
+)
 
 func getUniqueReleaseIdentifierName(releaseIdentifier *client.ReleaseIdentifier) string {
-	return releaseIdentifier.ReleaseNamespace + "_" + releaseIdentifier.ReleaseName + "_" + string(releaseIdentifier.ClusterConfig.ClusterId)
+	return releaseIdentifier.ReleaseNamespace + "_" + releaseIdentifier.ReleaseName + "_" + strconv.Itoa(int(releaseIdentifier.ClusterConfig.ClusterId))
 }
