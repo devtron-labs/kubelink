4d59ebd788c97776e466cef6d052c2c7539364c5
Prakash <prakash.kumar@devtron.ai>
2024-05-28 11:45:52 +0530

    fix: Handling all cases for 5xx (#148)
    * make ConvertHelmErrorToInternalError a generic func which returns a internal err, which handle all errors from helm either generic or specific error
    
    * fixes
    
    * code review fixes
    
    * include more dynamic errors in DynamicErrorMapping mapping
    
    * fix

--- error/utils.go
+++ error/utils.go
@@ -5,13 +5,35 @@ import (
 	"strings"
 )
 
+// ConvertHelmErrorToInternalError converts known error message from helm to internal error and also maps it with proper grpc code
 func ConvertHelmErrorToInternalError(err error) error {
+	genericError := getInternalErrorForGenericErrorTypes(err)
+	if genericError != nil {
+		return genericError
+	}
+	var internalError error
 	for helmErrMsg, internalErr := range helmErrorInternalErrorMap {
 		if strings.Contains(err.Error(), helmErrMsg) {
-			for internalErrMsg, internalErrCode := range internalErr {
-				return status.New(internalErrCode, internalErrMsg).Err()
-			}
+			internalError = status.New(internalErr.grpcCode, internalErr.errorMsg).Err()
 		}
 	}
+	return internalError
+}
+
+// getInternalErrorForGenericErrorTypes returns all those kinds of errors which are generic in nature and also dynamic, make sure to return all generic and dynamic errors from this func. instead of putting them in helmErrorInternalErrorMap
+func getInternalErrorForGenericErrorTypes(err error) error {
+	/*
+		for example:-
+			1. if namespace is not found err is:- namespace "ns1" not found,
+			2. in case ingress class not found error is of type ingress class: IngressClass.networking.k8s.io "ingress1" not found,
+			3. when some resource is forbidden then err can be of many formats one of which is:- Unable to continue with install: could not get information about the resource Ingress "prakash-1-prakash-env3-ingress" in namespace "prakash-ns3": ingresses.networking.k8s.io "prakash-1-prakash-env3-ingress" is forbidden...
+			etc..
+	*/
+	for errorMsg, code := range DynamicErrorMapping {
+		if strings.Contains(strings.ToLower(err.Error()), errorMsg) {
+			return status.New(code, err.Error()).Err()
+		}
+	}
+
 	return nil
 }
