9d9d6899ee7c5639789bc52748499d4e258835e3
RajeevRanjan27 <rajeev.ranjan@devtron.ai>
2024-06-03 17:11:51 +0530

    commiting the code for setting up in mac for referenc

--- fluxApplication/bean/bean.go
+++ fluxApplication/bean/bean.go
@@ -1,6 +1,9 @@
 package bean
 
-import "k8s.io/apimachinery/pkg/runtime/schema"
+import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
 
 const (
 	FluxKustomizationGroup   = "kustomize.toolkit.fluxcd.io"
@@ -14,14 +17,39 @@ const (
 	//DevtronCDNamespae            = "devtroncd"
 	//ArgoLabelForManagedResources = "app.kubernetes.io/instance"
 )
+const (
+	Destination string = "destination"
+	Server      string = "server"
+	STATUS      string = "status"
+	INVENTORY   string = "inventory"
+	ENTRIES     string = "entries"
+	ID          string = "id"
+	VERSION     string = "v"
+)
+const (
+	FieldSeparator  = "_"
+	ColonTranscoded = "__"
+)
 
 type FluxApplicationListDto struct {
-	Name         string `json:"appName"`
-	ClusterId    int    `json:"clusterId"`
-	ClusterName  string `json:"clusterName"`
-	Namespace    string `json:"namespace"`
-	HealthStatus string `json:"appStatus"`
-	SyncStatus   string `json:"syncStatus"`
+	Name           string `json:"appName"`
+	ClusterId      int    `json:"clusterId"`
+	ClusterName    string `json:"clusterName"`
+	Namespace      string `json:"namespace"`
+	HealthStatus   string `json:"appStatus"`
+	SyncStatus     string `json:"syncStatus"`
+	IsKustomizeApp bool   `json:"isKustomizeApp"`
+}
+
+type ObjMetadata struct {
+	Namespace string
+	Name      string
+	GroupKind schema.GroupKind
+}
+
+type ObjectMetadataCompact struct {
+	Id      string
+	Version string
 }
 
 var GvkForKustomizationFluxApp = schema.GroupVersionKind{
@@ -34,4 +62,47 @@ var GvkForHelmreleaseFluxApp = schema.GroupVersionKind{
 	Group:   FluxHelmReleaseGroup,
 	Kind:    FluxAppHelmreleaseKind,
 	Version: FluxHelmReleaseVersion,
+}
+
+type FluxAppDto struct {
+	Name      string `json:"appName"`
+	Namespace string `json:"namespace"`
+}
+
+type FluxpplicationDetailDto struct {
+	FluxAppDto *FluxApplicationListDto `json:"fluxAppDto"`
+	Manifest   map[string]interface{}  `json:"manifest"`
+}
+
+type FluxKustomization struct {
+	AppKsDetailDto   *FluxKsAppDetail          `json:"appDetailDto"`
+	Resources        []*FluxResource           `json:"fluxResource"`
+	Kustomizations   []*FluxKustomization      `json:"kustomizations"`
+	FluxHelmReleases []*FluxHelmReleases       `json:"fluxHelmReleases"`
+	ParentKsApp      string                    `json:"parentKsApp"`
+	Manifest         unstructured.Unstructured `json:"manifest"`
+}
+
+type FluxResource struct {
+	Gvk       schema.GroupVersionKind
+	Name      string
+	Namespace string
+}
+
+type FluxHelmReleases struct {
+	Resources   []*FluxHelmResource
+	ParentKsApp string
+	Name        string
+	Namespace   string
+}
+type FluxHelmResource struct {
+	Gvk       schema.GroupVersionKind
+	Name      string
+	Namespace string
+}
+
+type FluxKsAppDetail struct {
+	Name      string
+	Namespace string
+	GroupKind schema.GroupKind
 }
--- pkg/service/FluxApplicationService.go
+++ pkg/service/FluxApplicationService.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
+	"encoding/base64"
+	"fmt"
 	k8sUtils "github.com/devtron-labs/common-lib/utils/k8s"
 	k8sCommonBean "github.com/devtron-labs/common-lib/utils/k8s/commonBean"
 	"github.com/devtron-labs/kubelink/converter"
@@ -9,13 +13,18 @@ import (
 	client "github.com/devtron-labs/kubelink/grpc"
 	clusterRepository "github.com/devtron-labs/kubelink/pkg/cluster"
 	"go.uber.org/zap"
+	"io"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd/api/latest"
+	"strings"
 )
 
 type FluxApplicationService interface {
 	ListApplications(clusterIds []int) ([]*bean.FluxApplicationListDto, error)
 	GetFluxApplicationListForCluster(config *client.ClusterConfig) *client.FluxApplicationList
-	//GetAppDetail(resourceName, resourceNamespace string, clusterId int) (*bean.FluxApplicationListDto, error)
+	GetAppDetail(resourceName, resourceNamespace string, clusterId int) (*bean.FluxApplicationListDto, error)
 	//GetServerConfigIfClusterIsNotAddedOnDevtron(resourceResp *k8s.ManifestResponse, restConfig *rest.Config,
 	//	clusterWithApplicationObject clusterRepository.Cluster, clusterServerUrlIdMap map[string]int) (*rest.Config, error)
 	//GetClusterConfigFromAllClusters(clusterId int) (*k8s.ClusterConfig, clusterRepository.Cluster, map[string]int, error)
@@ -29,6 +38,10 @@ type FluxApplicationServiceImpl struct {
 	helmAppService    HelmAppService
 	converter         converter.ClusterBeanConverter
 }
+
+var (
+	NilObjMetadata = bean.FluxResource{}
+)
 
 func NewFluxApplicationServiceImpl(logger *zap.SugaredLogger,
 	clusterRepository clusterRepository.ClusterRepository,
@@ -82,7 +95,8 @@ func (impl *FluxApplicationServiceImpl) ListApplications(clusterIds []int) ([]*b
 			impl.logger.Errorw("error in getting rest config ", "err", err, "clusterId", clusterObj.Id)
 			return nil, err
 		}
-
+		resp, err := impl.k8sUtil.GetResource(context.Background(), "flux-system", "flux-system", bean.GvkForKustomizationFluxApp, restConfig)
+		fmt.Println(resp)
 		kustomizationResp, _, err := impl.k8sUtil.GetResourceList(context.Background(), restConfig, bean.GvkForKustomizationFluxApp, bean.AllNamespaces, true, nil)
 		if err == nil {
 			kustomizationAppLists := getApplicationListDtos(kustomizationResp.Resources.Object, clusterObj.ClusterName, clusterObj.Id, "")
@@ -138,12 +152,13 @@ func (impl *FluxApplicationServiceImpl) GetFluxApplicationListForCluster(config 
 	for _, item := range appListFinal {
 
 		fluxApp := &client.FluxApplication{
-			Name:         item.Name,
-			ClusterId:    int32(item.ClusterId),
-			ClusterName:  item.ClusterName,
-			Namespace:    item.Namespace,
-			HealthStatus: item.HealthStatus,
-			SyncStatus:   item.SyncStatus,
+			Name:           item.Name,
+			ClusterId:      int32(item.ClusterId),
+			ClusterName:    item.ClusterName,
+			Namespace:      item.Namespace,
+			HealthStatus:   item.HealthStatus,
+			SyncStatus:     item.SyncStatus,
+			IsKustomizeApp: item.IsKustomizeApp,
 		}
 		fluxAppFinal = append(fluxAppFinal, fluxApp)
 	}
@@ -175,14 +190,16 @@ func getApplicationListDtos(manifestObj map[string]interface{}, clusterName stri
 		rowsData := rowsDataRaw.([]interface{})
 		for _, rowData := range rowsData {
 			appListDto := &bean.FluxApplicationListDto{
-				ClusterId:   clusterId,
-				ClusterName: clusterName,
+				ClusterId:      clusterId,
+				ClusterName:    clusterName,
+				IsKustomizeApp: true,
 			}
 			rowDataMap := rowData.(map[string]interface{})
 			rowObject := rowDataMap[k8sCommonBean.K8sClusterResourceObjectKey].(map[string]interface{})
 			metadata := rowObject[k8sCommonBean.K8sClusterResourceMetadataKey].(map[string]interface{})
 
 			if FluxAppType == "HelmRelease" {
+
 				// Check for the existence and non-empty values of the required labels
 				labels := metadata["labels"].(map[string]interface{})
 				nameLabel, nameExists := labels["kustomize.toolkit.fluxcd.io/name"].(string)
@@ -190,6 +207,7 @@ func getApplicationListDtos(manifestObj map[string]interface{}, clusterName stri
 				if nameExists && nameLabel != "" && namespaceExists && namespaceLabel != "" {
 					continue
 				}
+				appListDto.IsKustomizeApp = false
 			}
 
 			rowCells := rowDataMap[k8sCommonBean.K8sClusterResourceCellKey].([]interface{})
@@ -214,4 +232,387 @@ func getApplicationListDtos(manifestObj map[string]interface{}, clusterName stri
 		}
 	}
 	return appLists
+}
+
+func (impl *FluxApplicationServiceImpl) GetAppDetail(AppDto bean.FluxApplicationListDto, config *client.ClusterConfig) error {
+
+	appDetail := &bean.FluxpplicationDetailDto{
+		FluxAppDto: &AppDto,
+	}
+	k8sClusterConfig := impl.converter.GetClusterConfigFromClientBean(config)
+	restConfig, err := impl.k8sUtil.GetRestConfigByCluster(k8sClusterConfig)
+	if err != nil {
+		impl.logger.Errorw("error in getting rest config by cluster Id", "err", err, "clusterId", config.ClusterId)
+		return err
+	}
+
+	if AppDto.IsKustomizeApp {
+		resp, err := impl.k8sUtil.GetResource(context.Background(), AppDto.Namespace, AppDto.Name, bean.GvkForKustomizationFluxApp, restConfig)
+		if err != nil {
+			impl.logger.Errorw("error in getting resource list", "err", err)
+			return err
+		}
+		if resp != nil && resp.Manifest.Object != nil {
+			appDetail.Manifest = resp.Manifest.Object
+			//fluxManagedResourceMap := getInventoryMap(resp.Manifest.Object)
+			//
+			//fluxKsSpecKubeConfig := getFluxKsSpecKubeConfig(resp.Manifest)
+			//
+			//AppDetailDto := &bean.FluxAppDto{
+			//	Name:      AppDto.Name,
+			//	Namespace: AppDto.Namespace,
+			//}
+			fluxKsApp := &bean.FluxKsAppDetail{
+				Name:      AppDto.Name,
+				Namespace: AppDto.Namespace,
+				GroupKind: schema.GroupKind{
+					Group: bean.FluxKustomizationGroup,
+					Kind:  bean.FluxAppKustomizationKind,
+				},
+			}
+
+			fluxKustomizationTree := bean.FluxKustomization{
+				AppKsDetailDto: fluxKsApp,
+				ParentKsApp:    "",
+			}
+			err1 := impl.getFluxManagedResourceTree(fluxKsApp, config, &fluxKustomizationTree)
+			if err1 != nil {
+				return err1
+			}
+
+			fmt.Println(fluxKustomizationTree)
+
+		}
+
+	}
+	return err
+}
+
+func (impl *FluxApplicationServiceImpl) getFluxManagedResourceTree(fluxKsApp *bean.FluxKsAppDetail, config *client.ClusterConfig, fluxAppTree *bean.FluxKustomization) error {
+
+	k8sClusterConfig := impl.converter.GetClusterConfigFromClientBean(config)
+	restConfig, err := impl.k8sUtil.GetRestConfigByCluster(k8sClusterConfig)
+	if err != nil {
+		return err
+	}
+	resp, err := impl.k8sUtil.GetResource(context.Background(), fluxKsApp.Namespace, fluxKsApp.Name, bean.GvkForKustomizationFluxApp, restConfig)
+	if err != nil {
+		impl.logger.Errorw("error in getting resource list", "err", err)
+		return err
+	}
+	if resp != nil && resp.Manifest.Object != nil {
+		fluxManagedResourceMap := getInventoryMap(resp.Manifest.Object)
+
+		fluxKsSpecKubeConfig := getFluxKsSpecKubeConfig(resp.Manifest.Object)
+
+		fmt.Println(fluxManagedResourceMap, fluxKsSpecKubeConfig)
+
+		fluxK8sResources := make([]*bean.FluxResource, 0)
+		for version, id := range fluxManagedResourceMap {
+			fluxResource, err := parseObjMetadata(id)
+			if err != nil {
+				fmt.Println("issue is here for some rreson , r", err)
+			}
+			fluxResource.Gvk.Version = version
+			if fluxResource.Gvk.Group == fluxKsApp.GroupKind.Group && fluxResource.Gvk.Kind == fluxKsApp.GroupKind.Kind && fluxResource.Name ==                                                                                                            && fluxResource.Namespace == namespace {
+				continue
+			}
+
+			if fluxResource.Gvk.Kind != bean.FluxAppHelmreleaseKind && fluxResource.Gvk.Kind != bean.FluxAppKustomizationKind {
+				fluxK8sResources = append(fluxK8sResources, &fluxResource)
+			}
+
+			if fluxResource.Gvk.Group == bean.FluxHelmReleaseGroup &&
+				fluxResource.Gvk.Kind == bean.FluxAppHelmreleaseKind {
+
+				fluxK8sResourcesOfHelm := &bean.FluxHelmReleases{
+					Name:        fluxResource.Name,
+					Namespace:   fluxResource.Namespace,
+					ParentKsApp: fluxAppTree.AppKsDetailDto.Name,
+				}
+
+				objects, err := impl.getHelmReleaseInventory(fluxK8sResourcesOfHelm.Name, fluxK8sResourcesOfHelm.Namespace, config)
+				if err != nil {
+					fmt.Println(err)
+				}
+				compact := true
+				compactGroup := "toolkit.fluxcd.io"
+				fluxHelmResources := make([]*bean.FluxHelmResource, 0)
+				for _, obj := range objects {
+					if compact && !strings.Contains(obj.Gvk.Group, compactGroup) {
+						continue
+					}
+					fmt.Println("namespace", obj.Namespace, "Name", obj.Name, "Group", obj.Gvk.Group, "Kind", obj.Gvk.Kind)
+					//ks.Add(object2.ObjMetadata(obj))
+					fluxHelmResources = append(fluxHelmResources, &bean.FluxHelmResource{
+						Gvk:       obj.Gvk,
+						Name:      obj.Name,
+						Namespace: obj.Namespace,
+					})
+				}
+				fluxK8sResourcesOfHelm.Resources = fluxHelmResources
+
+				fluxAppTree.FluxHelmReleases = append(fluxAppTree.FluxHelmReleases, fluxK8sResourcesOfHelm)
+
+			}
+
+			if fluxResource.Gvk.Group == bean.FluxKustomizationGroup &&
+				fluxResource.Gvk.Kind == bean.FluxAppKustomizationKind &&
+				// skip kustomization if it targets a remote clusters
+				fluxKsSpecKubeConfig == false {
+
+				//resp, err := impl.k8sUtil.GetResource(context.Background(), AppDto.Namespace, AppDto.Name, bean.GvkForKustomizationFluxApp, restConfig)
+				//if err != nil {
+				//	fmt.Println(err)
+				//}
+				//if resp != nil && resp.Manifest.Object != nil {
+				//	appDetail.Manifest = resp.Manifest.Object
+				//	fluxObjectMap := getInventoryMap(resp.Manifest.Object)
+				//	fluxKsSpecKubeConfig = false
+				//	appDetail.
+				//	fluxChildKsTree := bean.FluxKustomization{
+				//		AppDetailDto: ,
+				//		Resources: bean.FluxK8sResource{
+				//			ParentKsApp: AppDto.Name,
+				//		},
+				//	}
+				//
+				//}
+				fluxKsAppChild := &bean.FluxKsAppDetail{
+					Name:      fluxResource.Name,
+					Namespace: fluxResource.Namespace,
+					GroupKind: schema.GroupKind{
+						Group: bean.FluxKustomizationGroup,
+						Kind:  bean.FluxAppKustomizationKind,
+					},
+				}
+				fluxChildTree := &bean.FluxKustomization{
+					AppKsDetailDto: fluxKsAppChild,
+					ParentKsApp:    fluxResource.Name,
+				}
+				err = impl.getFluxManagedResourceTree(fluxKsAppChild, config, fluxChildTree)
+				fluxAppTree.Kustomizations = append(fluxAppTree.Kustomizations, fluxChildTree)
+
+			}
+
+		}
+		fluxAppTree.Resources = fluxK8sResources
+	}
+	return nil
+}
+
+func getFluxKsSpecKubeConfig(obj map[string]interface{}) bool {
+
+	kubeconfigSpec := false
+
+	return kubeconfigSpec
+}
+
+func parseObjMetadata(s string) (bean.FluxResource, error) {
+	index := strings.Index(s, bean.FieldSeparator)
+	if index == -1 {
+		return NilObjMetadata, fmt.Errorf("unable to parse stored object metadata: %s", s)
+	}
+	namespace := s[:index]
+	s = s[index+1:]
+	// Next, parse last field kind
+	index = strings.LastIndex(s, bean.FieldSeparator)
+	if index == -1 {
+		return NilObjMetadata, fmt.Errorf("unable to parse stored object metadata: %s", s)
+	}
+	kind := s[index+1:]
+	s = s[:index]
+	// Next, parse next to last field group
+	index = strings.LastIndex(s, bean.FieldSeparator)
+	if index == -1 {
+		return NilObjMetadata, fmt.Errorf("unable to parse stored object metadata: %s", s)
+	}
+	group := s[index+1:]
+	// Finally, second field name. Name may contain colon transcoded as double underscore.
+	name := s[:index]
+	name = strings.ReplaceAll(name, bean.ColonTranscoded, ":")
+	// Check that there are no extra fields by search for fieldSeparator.
+	if strings.Contains(name, bean.FieldSeparator) {
+		return NilObjMetadata, fmt.Errorf("too many fields within: %s", s)
+	}
+	// Create the ObjMetadata object from the four parsed fields.
+	id := bean.FluxResource{
+		Namespace: namespace,
+		Name:      name,
+		Gvk: schema.GroupVersionKind{
+			Group: group,
+			Kind:  kind,
+		},
+	}
+
+	return id, nil
+}
+
+func getInventoryMap(obj map[string]interface{}) map[string]string {
+
+	fluxManagedResourcesMap := make(map[string]string)
+
+	if statusRawObj, ok := obj[bean.STATUS]; ok {
+		statusObj := statusRawObj.(map[string]interface{})
+		if inventoryRawObj, ok2 := statusObj[bean.INVENTORY]; ok2 {
+			inventoryObj := inventoryRawObj.(map[string]interface{})
+			if entriesRawObj, ok3 := inventoryObj[bean.ENTRIES]; ok3 {
+				entriesObj := entriesRawObj.([]interface{})
+				for _, itemRawObj := range entriesObj {
+					itemObj := itemRawObj.(map[string]interface{})
+					var matadataCompact bean.ObjectMetadataCompact
+					if metadataRaw, ok4 := itemObj[bean.ID]; ok4 {
+						metadata := metadataRaw.(string)
+						matadataCompact.Id = metadata
+					}
+					if metadataVersionRaw, ok5 := itemObj[bean.VERSION]; ok5 {
+						metadataVersion := metadataVersionRaw.(string)
+						matadataCompact.Version = metadataVersion
+					}
+					fluxManagedResourcesMap[matadataCompact.Version] = matadataCompact.Id
+				}
+			}
+		}
+	}
+	return fluxManagedResourcesMap
+
+}
+
+func (impl *FluxApplicationServiceImpl) getHelmReleaseInventory(name string, namespace string, config *client.ClusterConfig) ([]bean.FluxHelmResource, error) {
+
+	k8sClusterConfig := impl.converter.GetClusterConfigFromClientBean(config)
+	restConfig, err := impl.k8sUtil.GetRestConfigByCluster(k8sClusterConfig)
+	resp, err := impl.k8sUtil.GetResource(context.Background(), namespace, name, bean.GvkForHelmreleaseFluxApp, restConfig)
+	if err != nil {
+		impl.logger.Errorw("error in getting resource list", "err", err)
+
+	}
+
+	//fmt.Println("spec-install-crds", hr.Spec.Install, "upgrade-data-crds", hr.Spec.Upgrade, "ns", hr.Namespace, "typemeta", hr.TypeMeta, "objectmeta", hr.ObjectMeta, "status", hr.Status)
+	// skip release if it targets a remote clusters
+	hrSpecKubeConfig := false
+	if hrSpecKubeConfig  {
+		return nil, nil
+	}
+	hrStatusStorageNamespace := "flux-system"
+
+	storageNamespace := hrStatusStorageNamespace
+	hrStatusHistoryLatest := "flux-system"
+
+	latestNamespace := hrStatusHistoryLatest
+	latest := "abcc"
+
+	if len(storageNamespace) == 0 || latest == "" {
+		// Skip release if it has no current
+		return nil, nil
+	}
+
+	storageKey := client.ObjectKey{
+		Namespace: storageNamespace,
+		Name:      fmt.Sprintf("sh.helm.release.v1.%s.v%v", latest.Name, latest.Version),
+	}
+
+	// to be implement using the helm secret code part
+
+	storageSecret := &corev1.Secret{}
+	if err := kubeClient.Get(ctx, storageKey, storageSecret); err != nil {
+		// skip release if it has no storage
+		if apierrors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to find the Helm storage object for HelmRelease '%s': %w", objectKey.String(), err)
+	}
+
+	releaseData, releaseFound := storageSecret.Data["release"]
+	if !releaseFound {
+		return nil, fmt.Errorf("failed to decode the Helm storage object for HelmRelease '%s'", objectKey.String())
+	}
+
+	// adapted from https://github.com/helm/helm/blob/02685e94bd3862afcb44f6cd7716dbeb69743567/pkg/storage/driver/util.go
+	var b64 = base64.StdEncoding
+	b, err := b64.DecodeString(string(releaseData))
+	if err != nil {
+		return nil, err
+	}
+	var magicGzip = []byte{0x1f, 0x8b, 0x08}
+	if bytes.Equal(b[0:3], magicGzip) {
+		r, err := gzip.NewReader(bytes.NewReader(b))
+		if err != nil {
+			return nil, err
+		}
+		defer r.Close()
+		b2, err := io.ReadAll(r)
+		if err != nil {
+			return nil, err
+		}
+		b = b2
+	}
+
+	// extract objects from Helm storage
+	var rls hrStorage
+	if err := json.Unmarshal(b, &rls); err != nil {
+		return nil, fmt.Errorf("failed to decode the Helm storage object for HelmRelease '%s': %w", objectKey.String(), err)
+	}
+
+	objects, err := ssautil.ReadObjects(strings.NewReader(rls.Manifest))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read the Helm storage object for HelmRelease '%s': %w", objectKey.String(), err)
+	}
+
+	// set the namespace on namespaced objects
+	for _, obj := range objects {
+		if obj.GetNamespace() == "" {
+			if isNamespaced, _ := apiutil.IsObjectNamespaced(obj, kubeClient.Scheme(), kubeClient.RESTMapper()); isNamespaced {
+				obj.SetNamespace(latest.Namespace)
+			}
+		}
+	}
+
+	result := object2.UnstructuredSetToObjMetadataSet(objects)
+	//fmt.Println("resources of helmrelease", objectKey.Name, objectKey.Namespace)
+
+	//for _, obj := range result {
+	//	fmt.Println("kind", obj.GroupKind.Kind, "Group", obj.GroupKind.Group, "Namespace", obj.Namespace, "Name", obj.Name)
+	//}
+	//fmt.Println("resources end  of helmrelease", objectKey.Name, objectKey.Namespace)
+	// search for CRDs managed by the HelmRelease if installing or upgrading CRDs is enabled in spec
+	if (hr.Spec.Install != nil && len(hr.Spec.Install.CRDs) > 0 && hr.Spec.Install.CRDs != helmv2.Skip) ||
+		(hr.Spec.Upgrade != nil && len(hr.Spec.Upgrade.CRDs) > 0 && hr.Spec.Upgrade.CRDs != helmv2.Skip) {
+		selector := client.MatchingLabels{
+			fmt.Sprintf("%s/name", helmv2.GroupVersion.Group):      hr.GetName(),
+			fmt.Sprintf("%s/namespace", helmv2.GroupVersion.Group): hr.GetNamespace(),
+		}
+		//fmt.Println(hr.GetName(), hr.GetNamespace())
+		crdKind := "CustomResourceDefinition"
+		var list apiextensionsv1.CustomResourceDefinitionList
+		if err := kubeClient.List(ctx, &list, selector); err == nil {
+			for _, crd := range list.Items {
+				found := false
+				for _, r := range result {
+					if r.Name == crd.GetName() && r.GroupKind.Kind == crdKind {
+						//fmt.Println(r.Name)
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					result = append(result, object2.ObjMetadata{
+						Name: crd.GetName(),
+						GroupKind: schema.GroupKind{
+							Group: apiextensionsv1.GroupName,
+							Kind:  crdKind,
+						},
+					})
+				}
+				fmt.Println(crd.Name, crd.Namespace)
+			}
+		}
+	}
+	for _, obj := range result {
+		fmt.Println("kind", obj.GroupKind.Kind, "Group", obj.GroupKind.Group, "Namespace", obj.Namespace, "Name", obj.Name)
+	}
+	//fmt.Println(result)
+
+	return result, nil
 }
